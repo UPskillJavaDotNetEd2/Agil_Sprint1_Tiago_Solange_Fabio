d867bcdb0dd29e7496006090fdd5762c8657d92a
tbarracha <ti.barracha@gmail.com>
2023-12-27 12:50:17 +0000

    Update ClientInput.java

--- src/main/java/org/upskill/ClientInput.java
+++ src/main/java/org/upskill/ClientInput.java
@@ -4,4 +4,11 @@ public class ClientInput {
     // 1) menu for each client
     // 2) client specific input
     // 3) validate input & catch exceptions
+
+    public Cliente StartClienteGenerator() {
+
+        // 1) sout: choose cliente
+        // 2) run cliente input method
+        return null;
+    }
 }
